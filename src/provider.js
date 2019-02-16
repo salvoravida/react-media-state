3a15a23092af16cc64ca2777cb8a1e6ac807a2d3
salvoravida <salvoravida@gmail.com>
2019-02-16 17:06:47 +0100

    MediaStateProvider

---
+++ src/provider.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import { MediaStateContext } from './context';
+import { isSameState } from './core';
+
+export class MediaStateProvider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.windowSizeListeners = [];
+    this.value = {
+      debug: props.debug,
+      mediaState: props.getMediaState(),
+      getMediaState: props.getMediaState,
+      subscribe: this.subscribe
+    };
+  }
+
+  componentDidMount = () => {
+    this.mounted = true;
+    window.addEventListener('resize', this.onWindowResize);
+  };
+
+  componentWillUnmount = () => {
+    this.mounted = false;
+    window.removeEventListener('resize', this.onWindowResize);
+  };
+
+  onWindowResize = () => {
+    if (!this.mounted) return;
+    const mediaState = this.props.getMediaState();
+    if (!isSameState(mediaState, this.value.mediaState)) {
+      this.value.mediaState = mediaState;
+      const freezedListeners = this.windowSizeListeners.slice(0);
+      ReactDOM.unstable_batchedUpdates(() => {
+        freezedListeners.forEach(c => c(mediaState));
+      });
+    }
+  };
+
+  subscribe = (callback) => {
+    this.windowSizeListeners.push(callback);
+    return () => this.windowSizeListeners.splice(this.windowSizeListeners.indexOf(callback), 1);
+  };
+
+  render() {
+    return (
+      <MediaStateContext.Provider value={this.value}>
+        {this.props.children}
+      </MediaStateContext.Provider>
+    );
+  }
+}
+
+MediaStateProvider.propTypes = {
+  getMediaState: PropTypes.func.isRequired,
+  children: PropTypes.any.isRequired,
+  debug: PropTypes.bool
+};
